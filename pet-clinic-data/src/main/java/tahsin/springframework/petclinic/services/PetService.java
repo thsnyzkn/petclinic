d70f339adbb4fd139c1f9deb4d73a9c43f1d094b
Tahsin Yazkan <tahsinyazkan@gmail.com>
2019-05-15 15:28:37 +0300

    Closes Issue #10.

--- pet-clinic-data/src/main/java/tahsin/springframework/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/tahsin/springframework/petclinic/services/PetService.java
@@ -2,13 +2,9 @@ package tahsin.springframework.petclinic.services;
 
 import tahsin.springframework.petclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
 
-    Pet findById(Long id);
-    Pet save(Pet pet);
-    Set<Pet> findAll();
 
 
 }
--- pet-clinic-data/src/main/java/tahsin/springframework/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/tahsin/springframework/petclinic/services/VetService.java
@@ -2,11 +2,7 @@ package tahsin.springframework.petclinic.services;
 
 import tahsin.springframework.petclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet,Long> {
 
-public interface VetService {
 
-    Vet findById(Long id);
-    Vet save(Vet pet);
-    Set<Vet> findAll();
 }
