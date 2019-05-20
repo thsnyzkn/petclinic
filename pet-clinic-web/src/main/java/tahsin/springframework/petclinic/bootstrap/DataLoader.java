d321c08a47ad23440f85df1bc22d53008b4545d4
Tahsin Yazkan <tahsinyazkan@gmail.com>
2019-05-20 14:07:03 +0300

    IDs are populated automatically. Closes #23

--- pet-clinic-web/src/main/java/tahsin/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/tahsin/springframework/petclinic/bootstrap/DataLoader.java
@@ -22,25 +22,21 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Tahsin");
         owner1.setLastName("Yazkan");
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Sezin");
         owner2.setLastName("Eriş Yazkan");
         ownerService.save(owner2);
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Hasan Tahsin");
         vet1.setLastName("Atayik");
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Server");
         vet2.setLastName("Serdaroğlu");
         vetService.save(vet2);
